78436f6c43550c7adec151a5bf5464ba9bbff666
Brian Cooke <brian.cooke@live.co.uk>
2018-03-28 18:07:28 +0100

    Final answer added

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -123,10 +123,9 @@ users["Avril"][:pets][0][:species]
 users["Erik"][:lottery_numbers].min()
 
 # 6. Return an array of Avril's lottery numbers that are even
-
-# lottery_array = []
-# lottery_array = users["Avril"][:lottery_numbers]
-# p lottery_array.each
+lottery_array = []
+lottery_array = users["Avril"][:lottery_numbers]
+return lottery_array.select(&:even?)
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"][:lottery_numbers].push(7)
